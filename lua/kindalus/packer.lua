4460cfdc89cb4ada312a131f196b2b2b30ed4451
Dzerjinski Lemos <dzerjinski.lemos@zafir.co.ao>
2024-05-15 09:22:38 +0100

    Wed May 15 09:22:38 WAT 2024

--- lua/kindalus/packer.lua
+++ lua/kindalus/packer.lua
@@ -59,22 +59,22 @@ return require('packer').startup(function(use)
 
 
    -- Goodies
-   use  {
-        "folke/noice.nvim",
-        event = "VimEnter",
-        opts = {
-             -- add any options here
-        },
-        requires = {
-             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-             "MunifTanjim/nui.nvim",
-             -- OPTIONAL:
-             --   `nvim-notify` is only needed, if you want to use the notification view.
-             --   If not available, we use `mini` as the fallback
-             "rcarriga/nvim-notify",
-        }
-   }
-
+--   use  {
+--        "folke/noice.nvim",
+--        event = "VimEnter",
+--        opts = {
+--             -- add any options here
+--        },
+--        requires = {
+--             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+--             "MunifTanjim/nui.nvim",
+--             -- OPTIONAL:
+--             --   `nvim-notify` is only needed, if you want to use the notification view.
+--             --   If not available, we use `mini` as the fallback
+--             "rcarriga/nvim-notify",
+--        }
+--   }
+--
 
    use {
         "folke/trouble.nvim",
--- lua/kindalus/remap.lua
+++ lua/kindalus/remap.lua
@@ -1,5 +1,17 @@
 vim.g.mapleader = "\\"
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 
+-- Move text
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Window split and navigation
+vim.keymap.set("n", "<leader>wq", "<C-w>q")
+vim.keymap.set("n", "<leader>wx", "<C-w><C-v>")
+vim.keymap.set("n", "<leader>wy", "<C-w>s")
+vim.keymap.set("n", "<leader>wh", "<C-w>h")
+vim.keymap.set("n", "<leader>wl", "<C-w>l")
+vim.keymap.set("n", "<leader>wj", "<C-w>j")
+vim.keymap.set("n", "<leader>wk", "<C-w>k")
+
+
