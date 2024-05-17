51b7ee7bd94f668b4f139ee3c5c4a283def0b12c
Dzerjinski Lemos <dzerjinski.lemos@zafir.co.ao>
2024-05-17 11:00:47 +0100

    sex 17 mai 2024 11:00:47 WAT

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,10 +5,6 @@ return {
           branch = "v3.x",
           config = function()
                local lsp_zero = require('lsp-zero')
-               lsp_zero.extend_lspconfig()
-
-
-
 
                lsp_zero.on_attach(function(client, bufnr)
                     -- see :help lsp-zero-keybindings
@@ -16,13 +12,33 @@ return {
                     lsp_zero.default_keymaps({ buffer = bufnr })
 
                     vim.keymap.set({ "n", "i", "x" }, "<C-.>", "<cmd>lua vim.lsp.buf.code_action()<CR>",
-                         { buffer = bufnr, noremap = true, silent = true })
+                         { buffer = bufnr, noremap = true, silent = true, desc = "Code Action" })
 
                     vim.keymap.set("v", "<C-.>", "<cmd>lua vim.lsp.buf.code_action()<CR>",
-                         { buffer = bufnr, noremap = true, silent = true })
+                         { buffer = bufnr, noremap = true, silent = true, desc = "Code Action" })
 
+                    vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions,
+                         { buffer = bufnr, desc = '[G]oto [D]efinition' })
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr, desc = '[G]oto [D]eclaration' })
+                    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references,
+                         { buffer = bufnr, desc = '[G]oto [R]eferences' })
+                    vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations,
+                         { buffer = bufnr, desc = '[G]oto [I]mplementation' })
 
-                    vim.keymap.set("n", "<leader>F", "<cmd>LspZeroFormat<cr>")
+                    vim.keymap.set('n', '<leader>D', require('telescope.builtin').lsp_type_definitions,
+                         { buffer = bufnr, desc = 'Type [D]efinition' })
+
+                    vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols,
+                         { buffer = bufnr, desc = '[D]ocument [S]ymbols' })
+
+                    vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+                         { buffer = bufnr, desc = '[W]orkspace [S]ymbols' })
+
+                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = bufnr, desc = '[R]e[n]ame' })
+
+                    vim.keymap.set("n", "<leader>cf", "<cmd>LspZeroFormat<cr>", { desc = "[C]ode [F]ormat" })
+
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, desc = 'Hover Documentation' })
                end)
 
 
@@ -55,6 +71,9 @@ return {
      {
           "williamboman/mason-lspconfig.nvim",
           config = function()
+
+               require("lsp-zero").extend_lspconfig()
+
                require("mason-lspconfig").setup({
 
                     ensure_installed = {
